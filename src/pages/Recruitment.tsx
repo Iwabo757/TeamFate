84db7ad7469235585e5f1844ba593cb18eee2e98
Iwabo757 <nickpallugna@gmail.com>
2026-08-07 18:39:02 -0500

    Fix recruitment rich text editor

--- src/pages/Recruitment.tsx
+++ src/pages/Recruitment.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
 import { supabase } from "../lib/supabase";
 
 type RecruitmentPageData = {
