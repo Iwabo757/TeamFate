3643f92b71f2a18eae39acf1c4e08e7af83dae79
Iwabo757 <nickpallugna@gmail.com>
2026-08-09 19:45:00 -0500

    Rebuild Horde Hunter layout

--- src/pages/HordeHunter.tsx
+++ src/pages/HordeHunter.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import monsters from "../data/monsters.json";
 
 type Season = "Spring" | "Summer" | "Autumn" | "Winter";
