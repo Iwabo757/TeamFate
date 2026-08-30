bd6333e0e9319afba80d41c48f64be4334579788
Iwabo757 <nickpallugna@gmail.com>
2026-08-29 19:34:21 -0500

    Fix ThemeProvider setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import "./App.css";
+
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
